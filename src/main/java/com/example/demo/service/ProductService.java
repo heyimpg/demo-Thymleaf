dab8905ce8668d77ae54e15609d0b4a71061a44b
hoang1998lqd <hoang1998lqd@gmail.com>
2022-11-03 09:09:03 +0700

    init

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Product;
+
+import java.util.List;
+
+
+public interface ProductService {
+
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+
+}
